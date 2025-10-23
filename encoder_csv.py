5ee92b211b904a485b619593afc0fb11fce889f6
shadowforce78 <planque.adam@gmail.com>
2025-10-23 12:47:33 +0200

    Ajout d'une vérification des arguments pour l'utilisation du script encoder_csv.py

--- encoder_csv.py
+++ encoder_csv.py
@@ -4,4 +4,8 @@ import sys
 from lib_csv import encode_file
 from lib_csv import restore_file
 
+if len(sys.argv) < 2:
+    print("Usage: python encoder_csv.py <input_file>")
+    sys.exit(1)
+
 encode_file(sys.argv[1])
